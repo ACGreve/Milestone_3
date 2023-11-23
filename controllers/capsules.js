8060ac34270040e4718be3b45afc42ce36d360b0
ACGreve <greve5921@gmail.com>
2023-11-22 21:02:44 -0500

    Added Mock login/signup pages

--- controllers/capsules.js
+++ controllers/capsules.js
@@ -2,17 +2,15 @@ const capsules = require('express').Router()
 const Capsule = require('../models/capsule')
 const capsuleSeedData = require('../models/capsuleSeedData')
 
-
 //Seed data route
 capsules.get('/data/seed', async (req, res)=>{
   await Promise.all([Capsule.deleteMany()])
    const capsules = await Capsule.insertMany(capsuleSeedData)
   const capsuleIds = capsules.map(capsule => capsule._id)
   res.redirect('/capsules')
-
 })
 
-//Home Route for all the movies
+//Home Route for all the capsules
 capsules.get('/', async(req, res)=>{//this route works fine
   res.render('index')
 })
