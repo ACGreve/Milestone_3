89120a8f20cf220b8afadbb2187eb72bd88a60c1
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 17:23:27 -0500

    organize views

--- views/capsules/index.jsx
+++ views/capsules/index.jsx
@@ -1,10 +1,9 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react")
+const Default = require("../default")
 
-function Index ({ }) {
-
-    return (
-        <html>
+function Index({}) {
+  return (
+    <html>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
@@ -13,11 +12,11 @@ function Index ({ }) {
       />
 
       <Default>
-         <h2>Open Capsules</h2>
-         <h2>Locked Capsules</h2>
+        <h2>Open Capsules</h2>
+        <h2>Locked Capsules</h2>
       </Default>
-      </html>
-    )
+    </html>
+  )
 }
 
 module.exports = Index
---
+++ views/capsules/newCapsule.jsx
@@ -0,0 +1,45 @@
+const React = require("react")
+const Def = require("../default")
+
+function new_form() {
+  return (
+    <Def title="Add a New Movie">
+      <body className="new">
+        <main>
+          <h1>Add a New Capsule</h1>
+          <form method="POST" action="/movies">
+            <div className="form-group">
+              <label htmlFor="title">Capsule title</label>
+              <input
+                className="form-control"
+                id="title"
+                name="title"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="date">Encapsulation Date</label>
+              <input className="form-control" id="date" name="date" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="place">Place</label>
+              <input className="form-control" id="place" name="place" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="image">Image</label>
+              <input className="form-control" id="image" name="image" />
+              <p>leave blank if no image is found</p>
+            </div>
+            <input
+              className="btn btn-primary"
+              type="submit"
+              value="Add Capsule"
+            />
+          </form>
+        </main>
+      </body>
+    </Def>
+  )
+}
+
+module.exports = new_form
