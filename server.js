0ecc70849e3be1250c01b78a1ac41b79b092ecf1
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 13:41:31 -0500

    remove deprecated options

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/capsules', require('./controllers/capsules'))
 app.use('/users', require('./controllers/users'))
 
 // db connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
