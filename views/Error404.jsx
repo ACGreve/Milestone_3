8e9b6574d92d4b6373438b47b9a1f759c9abdf77
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 17:22:27 -0500

    update error404 view

--- views/Error404.jsx
+++ views/Error404.jsx
@@ -1,12 +1,23 @@
+const React = require("react")
+const Default = require("./default")
 
 function Error404() {
-    return (
-        <main>
-            <h1>404: PAGE NOT FOUND</h1>
-            <p>Oops, sorry, we can't find this page!</p>
-            <img src={ process.env.REACT_APP_SERVER_URL + "images/kittykat.jpeg" } alt="cute cat pic" />
-        </main>
-    );
+  return (
+    <Default title="No capsules here...">
+      <body className="error404">
+        <h1>Oops, sorry, we can't find this page!</h1>
+        <img
+          src="https://www.accuwebhosting.com/blog/wp-content/uploads/2022/05/404-error-scaled.jpg"
+          alt="404error"
+          style={{ opacity: 0.7, width: "48vw", height: "auto" }}
+        />
+        <br />
+        <a href="/capsules">
+          <button className="btn btn-primary">Back to the Time Capsules</button>
+        </a>
+      </body>
+    </Default>
+  )
 }
 
-export default Error404;
+module.exports = Error404
