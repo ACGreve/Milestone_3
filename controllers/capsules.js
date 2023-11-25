ee2d347567fa9eb4146d8cc9617fab6704500f34
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 20:17:13 -0500

    add routes

--- controllers/capsules.js
+++ controllers/capsules.js
@@ -10,14 +10,39 @@ capsules.get('/data/seed', async (req, res)=>{
   res.redirect('/capsules')
 })
 
+//get signup page
+capsules.get("/signup", (req, res) => {
+  res.render("capsules/signupForm")
+})
+
+//get login page
+capsules.get("/login", (req, res) => {
+  res.render("capsules/loginForm")
+})
+
 //Home Route for all the capsules
-capsules.get('/', async(req, res)=>{//this route works fine
-  res.render('capsules/index')
+capsules.get('/', async(req, res)=>{
+  const capsules = await Capsule.find()
+  res.render("capsules/index", { capsules })
 })
 
 //get new page
 capsules.get("/new", (req, res) => {
   res.render("capsules/newCapsule")
+})
+
+//get capsule by index
+capsules.get("/:id", async (req, res) => {
+  try {
+    const capsule = await Capsule.findById(req.params.id)
+    if (!capsule) {
+      res.render("error404")
+    }
+    res.render("capsules/capsuleShow", { capsule })
+  } catch (err) {
+    console.log(err)
+    res.render("error404")
+  }
 })
 
 module.exports = capsules
