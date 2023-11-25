18a3b646f1e6efd1317093e5438c8f5e70f3f7d3
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 20:19:38 -0500

    update error404 view

--- views/Error404.jsx
+++ views/Error404.jsx
@@ -6,11 +6,6 @@ function Error404() {
     <Default title="No capsules here...">
       <body className="error404">
         <h1>Oops, sorry, we can't find this page!</h1>
-        <img
-          src="https://www.accuwebhosting.com/blog/wp-content/uploads/2022/05/404-error-scaled.jpg"
-          alt="404error"
-          style={{ opacity: 0.7, width: "48vw", height: "auto" }}
-        />
         <br />
         <a href="/capsules">
           <button className="btn btn-primary">Back to the Time Capsules</button>
