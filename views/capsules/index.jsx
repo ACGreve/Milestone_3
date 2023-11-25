953d5727574d7404c6b85d12882a80effc6a9ab8
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-24 20:18:45 -0500

    update capsules index view

--- views/capsules/index.jsx
+++ views/capsules/index.jsx
@@ -1,22 +1,46 @@
 const React = require("react")
 const Default = require("../default")
 
-function Index({}) {
+function index(data) {
+  //generates HTML elements for each capsule in the data.capsules array, creating a visual list of capsules with their titles, images, date, place.
+  let capsulesFormatted = data.capsules.map((capsule) => {
+    return (
+      <div
+        key={capsule.title}
+        className="col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-2"
+      >
+        <a href={`/capsules/${capsule.id}`}>
+          <img src={capsule.image} alt='Image not available' />
+        </a>
+        <h2>
+          <a href={`/capsules/${capsule.id}`}>{capsule.title}</a>
+        </h2>
+      </div>
+    )
+  })
   return (
-    <html>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossOrigin="anonymous"
-      />
-
-      <Default>
-        <h2>Open Capsules</h2>
-        <h2>Locked Capsules</h2>
-      </Default>
-    </html>
+    <Default title="Capsules">
+      <body className="capsules">
+        <main>
+          <h1>Capsules</h1>
+          <div className="row show-row">
+            <h2>Open Capsules</h2>
+            {capsulesFormatted}
+            <h2>Locked Capsules</h2>
+          </div>
+          <a href="/capsules/new">
+            <button className="btn btn-primary">Add a New capsule</button>
+          </a>
+        </main>
+      </body>
+      <style jsx="true">{`
+        .btn-primary {
+          margin-top: 1em;
+          margin-bottom: 3em;
+        }
+      `}</style>
+    </Default>
   )
 }
 
-module.exports = Index
+module.exports = index
