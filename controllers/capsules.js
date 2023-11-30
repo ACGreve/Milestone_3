a3b36ff458ae3d73b79745e427b54a4f2852a7c4
ACGreve <greve5921@gmail.com>
2023-11-29 21:34:09 -0500

    Added file uploading, needs work

--- controllers/capsules.js
+++ controllers/capsules.js
@@ -10,17 +10,7 @@ capsules.get('/data/seed', async (req, res)=>{
   res.redirect('/capsules')
 })
 
-//get signup page
-capsules.get("/signup", (req, res) => {
-  res.render("capsules/signupForm")
-})
-
-//get login page
-capsules.get("/login", (req, res) => {
-  res.render("capsules/loginForm")
-})
-
-//Home Route for all the capsules
+//Index Route for all the capsules
 capsules.get('/', async(req, res)=>{
   const capsules = await Capsule.find()
   res.render("capsules/index", { capsules })
@@ -29,11 +19,12 @@ capsules.get('/', async(req, res)=>{
 // Create new capsule with a specific status
 capsules.post("/", async (req, res) => {
     try {
-        const { name, date, place, image, status } = req.body; // Extract data including status
+        const { name, capsuleImage, date, place, image, status } = req.body; // Extract data including status
 
         // Create a new capsule with the provided data including the status
         const createCapsule = await Capsule.create({
             name,
+            capsuleImage,
             date,
             place,
             image,
@@ -55,6 +46,21 @@ capsules.get("/new", (req, res) => {
   res.render("capsules/newCapsule")
 })
 
+//get edit page
+capsules.get("/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params
+    const capsule = await Capsule.findById(id)
+    if (!capsule) {
+      res.render("error404")
+    }
+    res.render("capsules/editCapsule", { capsule })
+  } catch (err) {
+    console.log(err)
+    res.render("error404")
+  }
+})
+
 //get capsule by index
 capsules.get("/:id", async (req, res) => {
   try {
@@ -63,6 +69,36 @@ capsules.get("/:id", async (req, res) => {
       res.render("error404")
     }
     res.render("capsules/capsuleShow", { capsule })
+  } catch (err) {
+    console.log(err)
+    res.render("error404")
+  }
+})
+
+//edit capsule
+capsules.put("/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const capsule = await Capsule.findByIdAndUpdate(id, req.body)
+    if (!capsule) {
+      res.render("error404")
+    }
+    res.redirect(`/capsules/${req.params.id}`)
+  } catch (err) {
+    console.log(err)
+    res.render("error404")
+  }
+})
+
+//delete capsule by index
+capsules.delete("/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const capsule = await Capsule.findByIdAndDelete(id)
+    if (!capsule) {
+      res.render("error404")
+    }
+    res.redirect("/capsules")
   } catch (err) {
     console.log(err)
     res.render("error404")
