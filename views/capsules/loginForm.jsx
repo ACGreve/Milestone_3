3288beeac27f94bf0ebbaf4559e5804a56bff4e9
ACGreve <greve5921@gmail.com>
2023-12-02 14:24:23 -0500

    commented out login code

--- views/capsules/loginForm.jsx
+++ views/capsules/loginForm.jsx
@@ -6,6 +6,63 @@ import { ToastContainer, toast } from "react-toastify"
 // const React = require("react")
 const Default = require("../default")
 
+function login() {
+
+  return (
+    <div>
+      <Default>
+        <main>
+          <h1>Login</h1>
+          <form method="POST">
+          {/* <form onSubmit={(e) => handleSubmit(e)}> */}
+            <div className="row">
+              <div className="col-sm-6 form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="username"
+                  required
+                  // value={username}
+                  // onChange={(e) => setUsername(e.target.value)}
+                  className="form-control"
+                  id="username"
+                  name="username"
+                />
+              </div>
+              <div className="col-sm-6 form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  required
+                  //value={password}
+                  //onChange={(e) => setPassword(e.target.value)}
+                  className="form-control"
+                  id="password"
+                  name="password"
+                />
+              </div>
+            </div>
+            <input className="btn btn-primary" type="submit" value="Login" />
+            {/* <span>
+              Don't have an account ?<Link to="capsules/signup"> Signup </Link>
+            </span> */}
+          </form>
+          {/* <ToastContainer /> */}
+        </main>
+      </Default>
+    </div>
+  )
+}
+
+module.exports = login
+
+/*import React, { useState, useEffect } from "react"
+import { useCookies } from "react-cookie"
+// import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify"
+// const React = require("react")
+const Default = require("../default")
+
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -41,7 +98,7 @@ function LoginForm() {
       <Default>
         <main>
           <h1>Login</h1>
-          {/*<form method="POST">*/}
+          {/*<form method="POST">}
           <form onSubmit={(e) => handleSubmit(e)}> 
             <div className="row">
               <div className="col-sm-6 form-group">
@@ -74,14 +131,14 @@ function LoginForm() {
             <input className="btn btn-primary" type="submit" value="Login" />
             {/* <span>
               Don't have an account ?<Link to="capsules/signup"> Signup </Link>
-            </span> */}
+            </span> }
           </form>
-          {/* <ToastContainer /> */}
+          {/* <ToastContainer /> }
         </main>
       </Default>
     </div>
   )
 }
 
-export default LoginForm;
+export default LoginForm; */
 
--- views/navigation.jsx
+++ views/navigation.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import LoginForm from "./capsules/loginForm";
 
